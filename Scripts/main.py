07eda91a7d72afead804e620b383f12ff0fb966c
donkevlar <kevin.cayouette@gmail.com>
2024-01-30 10:54:11 -0500

    Added settings.py

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,24 +1,27 @@
-import os
+import settings
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
-import time
+import os
 from datetime import datetime
+import time
+from dotenv import load_dotenv
 
-#
 # File Imports
-from Bookshelf import bookshelfURL, bookshelfToken, \
-    bookshelf_test_connection, bookshelf_auth_test, bookshelf_listening_stats, DOCKER_VARS, \
-    bookshelf_libraries
+import Bookshelf as b
+
+# Logger Config
+logger = settings.logging.getLogger("bot")
 
 # Version Info
 versionNumber = 'Alpha_0.10'
 # Print Startup Time
 current_time = datetime.now()
+logger.info(f'Bot is Starting Up! | Startup Time: {current_time}')
+
 print("\nStartup time:", current_time)
 
 # Pulls from bookshelf file, if DOCKER == True, then this won't load local env file
-if not DOCKER_VARS:
+if not b.DOCKER_VARS:
     load_dotenv()
 
 # Get Discord Token from ENV
@@ -27,7 +30,7 @@ token = os.environ.get("DISCORD_TOKEN")
 print(f'\nStarting up bookshelf traveller v.{versionNumber}\n')
 
 # Start Server Connection Prior to Running Bot
-server_status_code = bookshelf_test_connection()
+server_status_code = b.bookshelf_test_connection()
 
 # Quit if server does not respond
 if server_status_code != 200:
@@ -48,7 +51,7 @@ else:
 # print(f'\nDiscord Token: {token}\n')
 
 # Will print username when successful
-auth_test = bookshelf_auth_test()
+auth_test = b.bookshelf_auth_test()
 
 time.sleep(0.5)
 
@@ -69,12 +72,13 @@ async def on_ready():
 async def sync_commands(ctx):
     await client.tree.sync()
     await ctx.send("Successfully Synced Commands")
+    logger.info(f' Successfully Synchronized Commands, for accuracy restart discord | Executed "sync"')
 
 
 @client.hybrid_command(name="listening-stats", description="Pulls your total listening time and other useful stats")
 async def totalTime(ctx):
     try:
-        formatted_sessions_string, data = bookshelf_listening_stats()
+        formatted_sessions_string, data = b.bookshelf_listening_stats()
         total_time = round(data.get('totalTime') / 60)  # Convert to Minutes
         if total_time >= 60:
             total_time = round(total_time / 60)  # Convert to hours
@@ -82,10 +86,13 @@ async def totalTime(ctx):
         else:
             message = f'Total Listening Time : {total_time} Minutes'
         await ctx.send(message)
-        print("sent: ", message)
+        logger.info(f' Successfully sent command: listening-stats')
+
     except Exception as e:
         ctx.send("Could not get complete this at the moment, please try again later.")
         print("Error: ", e)
+        logger.warning(
+            f'User:{client.user} (ID: {client.user.id}) | Error occured: {e} | Command Name: listening-stats')
 
 
 @client.hybrid_command(name="ping", description="Latency of the discord bot server to the discord central shard.")
@@ -93,14 +100,15 @@ async def ping(ctx):
     latency = round(client.latency * 1000)
     message = f'Discord BOT Server Latency: {latency} ms'
     await ctx.send(message)
+    logger.info(f' Successfully sent command: ping')
 
 
-@client.hybrid_command(name="all_libraries",
+@client.hybrid_command(name="all-libraries",
                        description="Display all current libraries with their ID and if only audiobooks")
 async def show_all_libraries(ctx):
     try:
         # Get Library Data from API
-        library_data = bookshelf_libraries()
+        library_data = b.bookshelf_libraries()
         formatted_data = ""
 
         # Iterate over each key-value pair in the dictionary
@@ -121,17 +129,19 @@ async def show_all_libraries(ctx):
         embed_message.add_field(name="Libraries", value=formatted_data, inline=False)
 
         await ctx.send(embed=embed_message)
+        logger.info(f' Successfully sent command: recent-sessions')
 
     except Exception as e:
         await ctx.send("Could not complete this at the moment, please try again later.")
+        logger.warning(f'User:{client.user} (ID: {client.user.id}) | Error occured: {e} | Command Name: all-libraries')
         print("Error: ", e)
 
 
-@client.hybrid_command(name="recent_sessions",
+@client.hybrid_command(name="recent-sessions",
                        description="Display last 5 sessions")
 async def show_recent_sessions(ctx):
     try:
-        formatted_sessions_string, data = bookshelf_listening_stats()
+        formatted_sessions_string, data = b.bookshelf_listening_stats()
 
         # Create Embed Message
         embed_message = discord.Embed(
@@ -153,10 +163,13 @@ async def show_recent_sessions(ctx):
             embed_message.add_field(name=display_title, value=session_info, inline=False)
 
         await ctx.send(embed=embed_message)
+        logger.info(f' Successfully sent command: recent-sessions')
 
     except Exception as e:
         await ctx.send("Could not complete this at the moment, please try again later.")
+        logger.warning(
+            f'User:{client.user} (ID: {client.user.id}) | Error occured: {e} | Command Name: recent-sessions')
         print("Error: ", e)
 
 
-client.run(os.environ.get("DISCORD_TOKEN"))
+client.run(settings.DISCORD_API_SECRET, root_logger=True)
---
+++ Scripts/settings.py
@@ -0,0 +1,46 @@
+import pathlib
+import os
+import logging
+import time
+from datetime import datetime
+from logging.config import dictConfig
+from dotenv import load_dotenv
+from Bookshelf import DOCKER_VARS
+
+if not DOCKER_VARS:
+    load_dotenv()
+
+DISCORD_API_SECRET = os.getenv("DISCORD_TOKEN")
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disabled_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"
+        },
+        "standard": {"format": "%(levelname)-10s - %(name)-15s : %(message)s"},
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+        "console2": {
+            "level": "WARNING",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+    },
+    "loggers": {
+        "bot": {"handlers": ["console"], "level": "INFO", "propagate": False},
+        "discord": {
+            "handlers": ["console2"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
+
+dictConfig(LOGGING_CONFIG)
