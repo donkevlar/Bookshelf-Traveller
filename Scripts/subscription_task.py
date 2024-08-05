24886ff55954adbea52c27baa818c7a2cc32d329
donkevlar <kevin.cayouette@gmail.com>
2024-08-05 14:42:58 -0400

    removed the task for now, going to look into another more reliable solution. Amended the name of the task to new-book-check.

--- Scripts/subscription_task.py
+++ Scripts/subscription_task.py
@@ -15,20 +15,20 @@ load_dotenv()
 logger = logging.getLogger("bot")
 
 # VARS
-TASK_FREQUENCY = s.TASK_FREQUENCY
+TASK_FREQUENCY = 5
 
 
 class SubscriptionTask(Extension):
     def __init__(self, bot):
         pass
 
-    def newBookTask(self, colour, task_frequency=TASK_FREQUENCY): # NOQA
+    def NewBookCheck(self, task_frequency=TASK_FREQUENCY):  # NOQA
         items_added = []
         libraries = c.bookshelf_libraries()
         current_time = datetime.now()
         bookshelfURL = os.environ.get("bookshelfURL")
 
-        time_minus_delta = current_time - timedelta(minutes=task_frequency)
+        time_minus_delta = current_time - timedelta(days=task_frequency)
         timestamp_minus_delta = int(time.mktime(time_minus_delta.timetuple()) * 1000)
 
         for name, (library_id, audiobooks_only) in libraries.items():
@@ -66,7 +66,6 @@ class SubscriptionTask(Extension):
                 embed_message = Embed(
                     title=f"Recently Added Book {count}",
                     description=f"Recently added books for {bookshelfURL}",
-                    color=colour,
                 )
                 embed_message.add_field(name="Title", value=title)
                 embed_message.add_field(name="Author", value=author)
@@ -77,62 +76,61 @@ class SubscriptionTask(Extension):
 
             return embeds
 
-    @Task.create(trigger=IntervalTrigger(minutes=TASK_FREQUENCY))
-    async def newBookCheck(self, ctx: InteractionContext):
-        embeds = self.newBookTask(colour=ctx.author.accent_color)
+    # @Task.create(trigger=IntervalTrigger(minutes=TASK_FREQUENCY))
+    # async def newBookTask(self, ctx: BaseContext):
+    #     embeds = self.NewBookCheck()
+    #     if embeds:
+    #         paginator = Paginator.create_from_embeds(self.bot, *embeds, timeout=120)
+    #         await paginator.send(ctx)
+    #
+    #     logger.info('Completed Newly Added Books Task!')
+
+    @slash_command(name="new-book-check",
+                   description="Enable/Disable a background task checking for newly added books.", dm_permission=True)
+    @slash_option(name="days", description="Lookback period, in days. Defaults to 1 day",
+                  opt_type=OptionType.INTEGER, autocomplete=False, required=False)
+    async def activeBookCheck(self, ctx: InteractionContext, days=1):
+        # if option == 'enable':
+        #     logger.info('Activating New Book Task! A message will follow.')
+        #     if not self.newBookCheck.running:
+        #         await ctx.send(
+        #             f"Activating New Book Task! This task will automatically refresh every *{TASK_FREQUENCY} minutes*!",
+        #             ephemeral=True)
+        #
+        #         self.newBookCheck.start(ctx)
+        #     else:
+        #         logger.warning('New book check task was already running, ignoring...')
+        #         await ctx.send('New book check task is already running, ignoring...', ephemeral=True)
+
+        embeds = self.NewBookCheck(task_frequency=days)
         if embeds:
+            logger.info(f'Recent books found in given search period of {days} days!')
             paginator = Paginator.create_from_embeds(self.bot, *embeds, timeout=120)
-            await paginator.send(ctx)
-
-        logger.info('Completed Newly Added Books Task!')
-
-    @slash_command(name="task-new-books",
-                   description="Enable/Disable a background task checking for newly added books.")
-    @slash_option(name="option", description="Enable, disable or immediately pull recently added books. ", opt_type=OptionType.STRING, autocomplete=True, required=True)
-    async def activeBookCheck(self, ctx: SlashContext, option: str):
-        if option == 'enable':
-            logger.info('Activating New Book Task! A message will follow.')
-            if not self.newBookCheck.running:
-                await ctx.send(
-                    f"Activating New Book Task! This task will automatically refresh every *{TASK_FREQUENCY} minutes*!",
-                    ephemeral=True)
-                await ctx.send(f"Important: *The task will be sent to where this message originates from!*",
-                               ephemeral=True)
-                self.newBookCheck.start(ctx)
-            else:
-                logger.warning('New book check task was already running, ignoring...')
-                await ctx.send('New book check task is already running, ignoring...', ephemeral=True)
-
-        elif option == 'view':
-            embeds = self.newBookTask(colour=ctx.author.accent_color)
-            if embeds:
-                logger.info(f'Recent books found! Task will refresh and execute in {TASK_FREQUENCY} minutes')
-                paginator = Paginator.create_from_embeds(self.bot, *embeds, timeout=120)
-                self.message = paginator.send(ctx)
-                await self.message
-            else:
-                await ctx.send(f"No recent books found in given lookback period of {TASK_FREQUENCY} minutes.", ephemeral=True)
-                logger.info(f'No recent books found. Task will refresh and execute in {TASK_FREQUENCY} minutes')
-
-        elif option == 'disable':
-            if self.newBookCheck.running:
-                await ctx.send("Disabled Task: *Recently Added Books*", ephemeral=True)
-                self.newBookCheck.stop()
-
-            else:
-                await ctx.send("The task is currently not active", ephemeral=True)
-
+            await paginator.send(ctx, ephemeral=True)
         else:
-            await ctx.send("Invalid option entered!", ephemeral=True)
+            await ctx.send(f"No recent books found in given search period of {days} days.",
+                           ephemeral=True)
+            logger.info(f'No recent books found.')
+
+        # elif option == 'disable':
+        #     if self.newBookCheck.running:
+        #         await ctx.send("Disabled Task: *Recently Added Books*", ephemeral=True)
+        #         self.newBookCheck.stop()
+        #
+        #     else:
+        #         await ctx.send("The task is currently not active", ephemeral=True)
+
+        # else:
+        #     await ctx.send("Invalid option entered!", ephemeral=True)
 
     # Autocomplete Functions ---------------------------------------------
 
-    @activeBookCheck.autocomplete("option")
-    async def autocomplete_book_check(self, ctx: AutocompleteContext):
-        choices = [
-            {"name": "enable", "value": "enable"},
-            {"name": "disable", "value": "disable"},
-            {"name": "view", "value": "view"}
-        ]
-
-        await ctx.send(choices=choices)
+    # @activeBookCheck.autocomplete("option")
+    # async def autocomplete_book_check(self, ctx: AutocompleteContext):
+    #     choices = [
+    #         {"name": "enable", "value": "enable"},
+    #         {"name": "disable", "value": "disable"},
+    #         {"name": "view", "value": "view"}
+    #     ]
+    #
+    #     await ctx.send(choices=choices)
