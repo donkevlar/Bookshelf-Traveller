124f845652c01b9267b3b3b6393446d697059180
donkevlar <kevin.cayouette@gmail.com>
2024-08-30 01:26:10 -0400

    made log outputs debug

--- Scripts/main.py
+++ Scripts/main.py
@@ -179,8 +179,8 @@ if __name__ == '__main__':
         db_additions.add_column_to_db(db_connection=wishlist_conn,table_name='wishlist', column_name='downloaded', secondary_execute=secondary_command)
 
     except Exception as e:
-        logger.error(f"Error occured while attempting to alter original databases")
-        logger.error(e)
+        logger.debug(f"Error occured while attempting to alter original databases")
+        logger.debug(e)
 
     # Start Bot
     bot.start(settings.DISCORD_API_SECRET)
